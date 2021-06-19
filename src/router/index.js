f011e3e479ba66c48be0ee9af5cbc005a282f7fd
meganingtyas-bit <55680606+meganingtyas-bit@users.noreply.github.com>
2021-06-19 14:47:02 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import homepage from '../view/homepage.vue'
 import calculator from '../view/calculator.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'homepage',
-    component: homepage
-  },
   {
     path: '/calculator',
     name: 'calculator',
